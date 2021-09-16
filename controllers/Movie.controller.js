2a31a86167dc9a51b50aa02088edc913b95d3fa5
Eslam Akram <eslam_akram91@yahoo.com>
2021-09-16 20:59:09 +0300

    finished

--- controllers/Movie.controller.js
+++ controllers/Movie.controller.js
@@ -1,9 +1,18 @@
 "use strict";
 const Movie = require("../models/Movie.model");
 const axios = require('axios');
+const Cache = require('../data/cacheData')
+let cache = new Cache();
 
 
 const movieController=  async (req, res)=>{
+      let currentDate = new Date();
+        let cacheDate =  cache.date.getDate();
+        console.log(`cache date : ${cacheDate}`);
+        if (cache.data.length > 0 && cacheDate ===currentDate.getDate() ){
+            res.json({"data":cache,"message":"data retrieved from the cache"});
+
+        }else{
    
     let city = req.query.cityName;
         let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
@@ -12,7 +21,12 @@ const movieController=  async (req, res)=>{
         let movieData = axiosRes.data.results.map(movie =>{
             return new Movie(movie.title, movie.overview, movie.popularity, movie.vote_average, movie.vote_count, movie.release_date, movie.poster_path);
         });
-        res.status(200).json(movieData);
+        cache.data = movieData
+        cache.date = currentDate
+        res.json({movieData, "message ":" data is coming from the api"});
+    }
     }
 
 module.exports=movieController;
+
+
